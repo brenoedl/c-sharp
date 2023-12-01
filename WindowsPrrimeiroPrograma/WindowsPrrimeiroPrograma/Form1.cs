185cbe4f023b893129edde99fc35a77c153721f1
Breno170 <96654144+brenoedl@users.noreply.github.com>
2023-12-01 10:24:05 -0300

    Windows Prrimeiro Programa

---
+++ WindowsPrrimeiroPrograma/WindowsPrrimeiroPrograma/Form1.cs
@@ -0,0 +1,15 @@
+namespace WindowsPrrimeiroPrograma
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            lblMsg.Text = "Olá, Mundo!";
+        }
+    }
+}
